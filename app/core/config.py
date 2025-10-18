07c815f822e0165fa41b42e3bebf07314c8be1ef
Sbak729 <sench729@gmail.com>
2025-10-18 10:15:00 +0300

    refactor: adding pincone api key  validation

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    pinecone_api_key: str
+
 
     class Config:
         env_file = ".env"
