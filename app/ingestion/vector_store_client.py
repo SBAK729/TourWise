4846a7b45003e47963601bdd47951d8c7e09c202
Sbak729 <sench729@gmail.com>
2025-10-20 15:39:15 +0300

    refactor: updated

--- app/ingestion/vector_store_client.py
+++ app/ingestion/vector_store_client.py
@@ -1,36 +1,27 @@
 import os
 from pinecone import Pinecone, ServerlessSpec
 from dotenv import load_dotenv
-import os
-
 from tqdm import tqdm
 
-
 load_dotenv()
+
 class VectorStoreClient:
-    def __init__(self, index_name: str = "tourwise"):
+    def __init__(self, index_name: str = "tourwise", embedding_dim: int = 384):
         pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 
         if not pc.has_index(index_name):
             pc.create_index(
                 name=index_name,
-                dimension=384,
+                dimension=embedding_dim,
                 metric="cosine",
                 spec=ServerlessSpec(cloud="aws", region="us-east-1"),
             )
 
         self.index = pc.Index(index_name)
 
-
-    def upsert(self, items: list[dict]):
-        batch = []
-        for item in tqdm(items, desc="Upserting to Pinecone"):
-            vector = (item["id"], item["embedding"], item["metadata"])
-            batch.append(vector)
-            if len(batch) >= 100:
-                self.index.upsert(vectors=batch)
-                batch = []
-        if batch:
+    def upsert(self, items: list[dict], batch_size: int = 100):
+        for i in tqdm(range(0, len(items), batch_size), desc="Upserting to Pinecone"):
+            batch = [(item["id"], item["embedding"], item["metadata"]) for item in items[i:i+batch_size]]
             self.index.upsert(vectors=batch)
 
     def query(self, vector, top_k=5, filters=None):
