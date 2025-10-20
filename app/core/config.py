a4fa72d71bc11873325193cc221668502848ddf8
Sbak729 <sench729@gmail.com>
2025-10-20 15:37:29 +0300

    refactor: update

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     pinecone_api_key: str
-
+    GROQ_API_KEY: str
 
     class Config:
         env_file = ".env"
