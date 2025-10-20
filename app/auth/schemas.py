c5c7a5fe8574c4c8489bc68979ff8bd711afa6b9
Sbak729 <sench729@gmail.com>
2025-10-20 15:35:35 +0300

    refactor: adding query schema

--- app/auth/schemas.py
+++ app/auth/schemas.py
@@ -3,6 +3,11 @@ from pydantic import BaseModel, EmailStr, constr
 class UserCreate(BaseModel):
     email: EmailStr
     password: constr(min_length=8, max_length=72)
+
 class Token(BaseModel):
     access_token: str
     token_type: str = "bearer"
+
+class Query(BaseModel):
+    user_input: str
+    user_id: str
